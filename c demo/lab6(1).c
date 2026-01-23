9a91b0ea0987572275e4e28fd8e1983d980eb87f
JEET_THAKAR <jeetthakar91@gmail.com>
2026-01-23 19:52:47 +0530

    Add files via upload

---
+++ c demo/lab6(1).c
@@ -0,0 +1,10 @@
+//Create, declare and initialise the structure employee
+#include<stdio.h>
+
+struct employee{
+	   char employee_name;
+	   int employee_id;
+	   int employee_salary;
+};
+
+struct employee emp1;
